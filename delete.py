817b989658f2cca11d982a98bd73625b03631ff6
Carl <cknutson12@gmail.com>
2024-05-01 23:31:20 -0500

    again

--- delete.py
+++ delete.py
@@ -15,4 +15,5 @@
 #     for episode in episodes:
 #         print(requests.post(url=episode['CmdURL'], params={'cmd':'delete'}).status_code)
 
-print(['5 Eyewitness', 'Canada'])
+print('5 Eyewitness')
+print('Canada')
