c12337aeeadba2d57d5b0160c1e851932502e224
Carl <cknutson12@gmail.com>
2024-05-01 22:16:52 -0500

    initial commit

---
+++ delete.py
@@ -0,0 +1,18 @@
+# import requests
+
+# # https://info.hdhomerun.com/info/dvr_api:deleting_recordings
+
+# all_recorded_file_info = requests.get('http://hdhr-<UNIQUE-ID>.local/recorded_files.json').json()
+
+# # http://hdhr-<UNIQUE-ID>.local/recorded_files.json, EpisodesURL
+# mass_delete_urls = [
+
+# ]
+
+# for episode_url in mass_delete_urls:
+#     episodes = requests.get(episode_url).json()
+    
+#     for episode in episodes:
+#         print(requests.post(url=episode['CmdURL'], params={'cmd':'delete'}).status_code)
+
+print("hello, world")
