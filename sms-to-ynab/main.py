92bd9210a7a3ed34db1dbd7879f8288558a0347d
carlknutson <cknutson12@gmail.com>
2025-01-21 23:24:58 -0600

    `SMS_to_YNAB` script to retrieve unapproved transaction count

--- sms-to-ynab/main.py
+++ sms-to-ynab/main.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import json
 import os
 import requests
 import sys
@@ -8,16 +7,30 @@ ynab_api_url = 'https://api.ynab.com/v1'
 ynab_token = os.environ['ynab_token']
 ynab_budget_name = os.environ['ynab_budget_name']
 
-def add_transaction(account_name, message):
+def get_budget() -> dict:
     budgets = requests.get(url=f'{ynab_api_url}/budgets', params={'include_accounts': 'true'}, headers={'Authorization': f'Bearer {ynab_token}'}).json()['data']['budgets']
-    budget_id = None
-    account_id = None
     for budget in budgets:
         if budget['name'] == ynab_budget_name:
-            budget_id = budget['id']
-            for account in budget['accounts']:
-                if account['name'] == account_name:
-                    account_id = account['id']
+            return budget
+    
+    raise Exception(f"Unexpected error, unable to retrieve budget for {ynab_budget_name}")
+
+
+def get_unapproved_count() -> int:
+    budget = get_budget()
+    budget_id = budget['id']
+
+    transactions = requests.get(url=f'{ynab_api_url}/budgets/{budget_id}/transactions', params={'type': 'unapproved'}, headers={'Authorization': f'Bearer {ynab_token}'}).json()['data']['transactions']
+    print(len(transactions))
+
+def add_transaction(account_name, message):
+    budget = get_budget()
+    budget_id = budget['id']
+    account_id = None
+
+    for account in budget['accounts']:
+        if account['name'] == account_name:
+            account_id = account['id']
     
     if not account_id:
         print(f'No account found with name {account_name}.')
@@ -43,25 +56,23 @@ def add_transaction(account_name, message):
 def get_accounts():
     accounts = []
 
-    budgets = requests.get(url=f'{ynab_api_url}/budgets', params={'include_accounts': 'true'}, headers={'Authorization': f'Bearer {ynab_token}'}).json()['data']['budgets']
-    for budget in budgets:
-        if budget['name'] == ynab_budget_name:
-            for account in budget['accounts']:
-                if not (account['closed'] or account['deleted']):
-                    accounts.append(account['name'])
+    budget = get_budget()
+    for account in budget['accounts']:
+        if not (account['closed'] or account['deleted']):
+            accounts.append(account['name'])
     print(accounts)
 
 def get_payees():
     payees = []
 
-    budgets = requests.get(url=f'{ynab_api_url}/budgets', headers={'Authorization': f'Bearer {ynab_token}'}).json()['data']['budgets']    
-    for budget in budgets:
-        if budget['name'] == ynab_budget_name:
-            response = requests.get(url=f'{ynab_api_url}/budgets/{budget["id"]}/payees', headers={'Authorization': f'Bearer {ynab_token}'}).json()['data']['payees']
+    budget = get_budget()
+    budget_id = budget['id']
 
-            for payee in response:
-                if not (payee['deleted'] or payee['transfer_account_id']):
-                    payees.append(payee['name'])
+    response = requests.get(url=f'{ynab_api_url}/budgets/{budget_id}/payees', headers={'Authorization': f'Bearer {ynab_token}'}).json()['data']['payees']
+
+    for payee in response:
+        if not (payee['deleted'] or payee['transfer_account_id']):
+            payees.append(payee['name'])
     return payees
 
 def main():
@@ -78,6 +89,8 @@ def main():
             print(f'Action: {action}, requires an account and message')
     elif action == 'get_accounts':
         get_accounts()
+    elif action == 'get_unapproved_count':
+        get_unapproved_count()
     else:
         print(f'Action: {action}, is not implemented.')
 
