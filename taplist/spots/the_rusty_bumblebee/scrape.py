f9af769461e65b5c232b141c6990827dc17ba3ff
Carl <cknutson12@gmail.com>
2025-01-26 21:27:53 -0600

    add workflow file

--- taplist/spots/the_rusty_bumblebee/scrape.py
+++ taplist/spots/the_rusty_bumblebee/scrape.py
@@ -13,7 +13,7 @@ def scrape_beer_menu(url):
             # Wait for the page to load completely
             page.wait_for_timeout(5000)  # Adjust the timeout if necessary
 
-            beer_elements = page.locator(".item")
+            beer_elements = page.locator(".section").nth(0).locator(".item")
 
             beer_menu = []
 
@@ -21,20 +21,22 @@ def scrape_beer_menu(url):
             for i in range(count):
                 beer = beer_elements.nth(i)
                 name = beer.locator(".item-name").locator("span").nth(1).text_content().strip()
-                type = beer.locator(".item-name").locator(".item-category").text_content().strip() if beer.locator(".item-name").locator(".item-category").count() > 0 else "N/A"
+                beer_type = beer.locator(".item-name").locator(".item-category").text_content().strip() if beer.locator(".item-name").locator(".item-category").count() > 0 else "N/A"
                 abv = beer.locator(".item-abv").text_content().strip() if beer.locator(".item-abv").count() > 0 else "N/A"
                 brewery = beer.locator(".brewery").text_content().strip() if beer.locator(".brewery").count() > 0 else "N/A"
                 description = beer.locator(".item-description").locator("p").text_content().strip() if beer.locator(".item-description").count() > 0 else "N/A"
 
                 beer_menu.append({
-                    'Name': name,
-                    'ABV': abv,
-                    'Brewery': brewery,
-                    'Type': type,
-                    'Description': description
+                    'Name': f'"{name}"',
+                    'ABV': f'"{abv}"',
+                    'Brewery': f'"{brewery}"',
+                    'Type': f'"{beer_type}"',
+                    'Description': f"'{description.replace("\n", "")}'"
                 })
 
             browser.close()
+
+            beer_menu = sorted(beer_menu, key=lambda x: x["Name"])
             return beer_menu
 
     except Exception as e:
@@ -45,13 +47,15 @@ if __name__ == "__main__":
     url = "https://therustybumblebee.com/on-tap/"
     beers = scrape_beer_menu(url)
 
+    print("taps:")
+
     if beers:
-        print("\nBeer Menu:\n")
         for beer in beers:
-            print(f"Name: {beer['Name']}")
-            print(f"ABV: {beer['ABV']}")
-            print(f"Brewery: {beer['Brewery']}")
-            print(f"Type: {beer['Type']}\n")
-            print(f"Description: {beer['Description']}")
+            print(f"  - name: {beer['Name']}")
+            print(f"    abv: {beer['ABV']}")
+            print(f"    brewery: {beer['Brewery']}")
+            print("    description: >")
+            print(f"      {beer['Description']}")
+            print(f"    type: {beer['Type']}")
     else:
-        print("No beers found or an error occurred.")
+        raise
